b0ac98177e83ebdac5c657fd0a3e5dbbc16540c6
Mohamed Ahmed <mohamedaltaweel7@gmail.com>
2018-03-12 01:57:42 +0200

    FV

---
+++ JS/data.js
@@ -0,0 +1,46 @@
+// Model data
+
+var locations = [
+  {
+      title: 'Egyption Museum',
+      location: {
+          lat: 30.047847,
+          lng: 31.233649
+      }
+  },
+  {
+      title: 'Rayan Valley',
+      location: {
+          lat: 29.302475,
+          lng: 30.853801
+      }
+  },
+  {
+      title: 'Smart Village',
+      location: {
+          lat: 30.070007,
+          lng: 31.018942
+      }
+  },
+  {
+      title: 'Moez Street',
+      location: {
+          lat: 30.050931,
+          lng: 31.261575
+      }
+  },
+  {
+      title: 'GUC',
+      location: {
+          lat: 29.986638,
+          lng: 31.441422
+      }
+  },
+  {
+      title: 'Sphinx',
+      location: {
+          lat: 29.975269,
+          lng: 31.137567
+      }
+  }
+];
